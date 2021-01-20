669ffe9e149e2d1f095e1d1101d571d9f12499bc
Antony <vontonnie@gmail.com>
2021-01-20 18:34:20 +0300

    Initialize Pain Object with basic styles

--- app/src/main/java/com/example/android/customfancontroller/DialView.kt
+++ app/src/main/java/com/example/android/customfancontroller/DialView.kt
@@ -1,7 +1,9 @@
 package com.example.android.customfancontroller
 
 import android.content.Context
+import android.graphics.Paint
 import android.graphics.PointF
+import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
 
@@ -20,18 +22,30 @@ private enum class FanSpeed(val label: Int) {
 
 private const val RADIUS_OFFSET_LABEL = 30
 private const val RADIUS_OFFSET_INDICATOR = -35
-
+/*JvmOverloads annotation instructs the Kotlin compiler to generate
+* overloads for this function that substitute default parameter values*/
 
 //Add View constructor using @JVMOverloads
 class DialView @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : View(
         context, attrs, defStyleAttr) {
-
-            private var radius = 0.0f //current radius of the the circle
+    /*initialize these values here instead of when a view is actuall drawn to ensure that the actual drawing
+     step runs as fast as possible*/
+    private var radius = 0.0f //current radius of the the circle
     private var fanSpeed = FanSpeed.OFF //The active selection, by default off
 
     //position variable which wll be used to draw label and indicator circle position on the screen
 
     private val pointPosition: PointF = PointF(0.0f, 0.0f)
+
+    //initialize Paint Object with basic styles
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = 55.0f
+        typeface = Typeface.create("", Typeface.BOLD)
+    }
 
 }
